71ac3f04417948cae0f6f9f2954c68eb00a82960
Julius Simutis <julius@ivejas.lt>
2020-12-21 19:40:41 +0200

    Paskaita 11

---
+++ paskaita11/functions.js
@@ -0,0 +1,8 @@
+"use strict"
+
+function test() {
+  console.log('test');
+  document
+    .getElementById('container')
+    .style.backgroundColor = 'yellow';
+}
---
+++ paskaita11/logic.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let und;
+
+
+document
+  .getElementById('container')
+  .style.backgroundColor = 'red';
+
+let arrayVar = [10, 10, 10];
