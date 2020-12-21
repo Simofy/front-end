4e91fb96d958edd81114f8f229d58f2f4967f4cb
Julius Simutis <julius@ivejas.lt>
2020-12-21 21:04:35 +0200

    Adding js function random color

--- paskaita11/functions.js
+++ paskaita11/functions.js
@@ -1,8 +1,30 @@
 "use strict"
 
-function test() {
-  console.log('test');
-  document
-    .getElementById('container')
-    .style.backgroundColor = 'yellow';
+
+let arrayOfColors = [
+  'red', // index 0
+  'yellow', // index 1
+  'green', // index 2
+  'purple',
+  'black',
+]
+
+let index = 0;
+
+function changeColor() {
+  let randomLeft = Math.random() * 100;
+  let element = document
+    .getElementById('container');
+  element.style.backgroundColor = arrayOfColors[index];
+  element.style.left = randomLeft + '%';
+  console.log(arrayOfColors[index], index);
+  // index += 1;
+  index = (index + 1) % arrayOfColors.length;
 }
+
+function onLoad() {
+  document.getElementById('buttonChangeColor').onclick = changeColor;
+}
+
+window.onload = onLoad;
+
--- paskaita11/logic.js
+++ paskaita11/logic.js
@@ -1,11 +1,1 @@
 "use strict"
-
-let und;
-
-let labas = "test";
-
-document
-  .getElementById('container')
-  .style.backgroundColor = 'red';
-
-let arrayVar = [10, 10, 10];
