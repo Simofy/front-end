e3de24799fe76c545470f6f886f3fabef33c5130
Julius Simutis <julius@ivejas.lt>
2021-01-05 19:15:02 +0200

    Updating cors

--- server/main.js
+++ server/main.js
@@ -12,6 +12,12 @@ app.listen(appPort, () => {
 });
 
 app.get('/api/generate-shopping-cart', (req, res, next) => {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
   const { limit, exclude } = req.query;
   const total = limit ? limit : Math.floor(Math.random() * 100 + 10);
   const response = [];
