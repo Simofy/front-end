48d3836456f9174530bbff8935935ed2fb194c74
Julius Simutis <julius@ivejas.lt>
2021-01-05 16:37:08 +0200

    Refreshing server

--- server/main.js
+++ server/main.js
@@ -1,19 +1,18 @@
 require('dotenv').config()
 const { uniqueNamesGenerator, starWars } = require('unique-names-generator');
-const assert = require('assert');
 const express = require("express");
 const app = express();
 
 const appPort = 2020;
 
-app.use(express.static('../'))
+app.use(express.static('public'));
 
 app.listen(appPort, () => {
   console.log((new Date()).toISOString(), `Hello ssh! Port: ${appPort}`);
 });
 
 app.get('/api/generate-shopping-cart', (req, res, next) => {
-  const { limit } = req.query;
+  const { limit, exclude } = req.query;
   const total = limit ? limit : Math.floor(Math.random() * 100 + 10);
   const response = [];
   for (let i = 0; i < total; i++) {
