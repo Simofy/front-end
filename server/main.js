e4a891e22a1ee5336b116f3b81cc8cdfe42cf109
Julius Simutis <julius@ivejas.lt>
2021-01-07 21:46:56 +0200

    Api for lakes

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
-require('dotenv').config()
+require('dotenv').config();
 const { uniqueNamesGenerator, starWars } = require('unique-names-generator');
 const express = require("express");
+const dataEzerai = require('./data');
 const app = express();
 
 const appPort = 3000;
@@ -10,6 +11,17 @@ app.use(express.static(__dirname + '/public'));
 app.listen(appPort, () => {
   console.log((new Date()).toISOString(), `Hello ssh! Port: ${appPort}`);
 });
+
+app.get('/api/get-random-lake', (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+  const index = Math.floor(dataEzerai.length * Math.random());
+  res.json(dataEzerai[index]);
+
+})
 
 app.get('/api/generate-shopping-cart', (req, res, next) => {
 
@@ -33,3 +45,12 @@ app.get('/api/generate-shopping-cart', (req, res, next) => {
   }
   res.json(response);
 });
+
+
+/**
+ * (1 * css + 1 * js && js > 0.1 && js < 0.9) *
+ * original idea *
+ * process + 0.2 *
+ * extra work
+ *
+ */
